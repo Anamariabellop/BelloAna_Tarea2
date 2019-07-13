0a6dc99dc378c6fb7853b839d4cbc863fa0aadd1
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-13 16:59:57 -0500

    Leapfrog inicial

--- ODEs.cpp
+++ ODEs.cpp
@@ -8,6 +8,7 @@ double dvydt(double tiempo, double y0, double vy0);
 double dydt(double tiempo, double x0, double vx0);
 double dxdt(double tiempo, double y0, double vy0);
 void euler(double a, double b,double xinicial, double yinicial, double vxini, double vyini, double delta, int npuntos, string filename);
+void leapfrog(double a, double b,double xinicial, double yinicial,double vxini, double vyini, double delta, int npuntos, string filename);
 
 //void euler(double xinicial, double yinicial, double delta, int npuntos, string filename);
 
@@ -19,6 +20,7 @@ int main()
 	double vy0= 0.606; //UA/yr
 	double M=1.98*pow(10,30);
 	euler(1,100,x0,y0,vx0,vy0,0.1,100,"euler.txt");
+	leapfrog(1,100,x0,y0,vx0,vy0,0.1,100,"leapfrog.txt");
 
 	return 0;
 }
@@ -83,5 +85,47 @@ void euler(double a, double b,double xinicial, double yinicial,double vxini, dou
 	for(int i=0; i<npuntos; i++){
 		outfile << t[i] <<"  " << x[i] << "   " << y[i]<< "   " << vx[i] << "   " << vy[i] << endl;
 	}
+	outfile.close();
+}
+
+void leapfrog(double a, double b,double xinicial, double yinicial,double vxini, double vyini, double delta, int npuntos, string filename){
+
+	ofstream outfile;
+	outfile.open(filename);
+
+	//Arreglos.
+	double t[npuntos];
+	t[0]=0.0;
+
+	double x[npuntos];
+	x[0]=xinicial;
+
+	double y[npuntos];
+	y[0]=yinicial;
+
+	double vx[npuntos];
+	vx[0]=vxini;
+
+	double vy[npuntos];
+	vy[0]=vyini;
+
+	double dt= (b-a)/(npuntos-1); //dt para el linspace.
+
+
+	for(int i=1; i<npuntos; i++){ //Linspace para el tiempo.
+		t[i]= t[i-1]+dt;
+	}
+
+	for(int i=1; i<npuntos; i++){ //Calculo de x, y, vx y vy.
+		x[i]= x[i-1] +  (0.5*delta*dxdt(t[i-1],x[i-1],vx[i-1]));
+		y[i]= y[i-1] +  (0.5*delta*dydt(t[i-1],y[i-1],vy[i-1]));
+		vx[i]= vx[i-1] + (delta*dvxdt(t[i-1],x[i-1],vx[i-1]));
+		vy[i]= vy[i-1] + (delta*dvydt(t[i-1],y[i-1],vy[i-1]));
+		}
+
+	for(int i=0; i<npuntos; i++){
+		outfile << t[i] <<"  " << x[i] << "   " << y[i]<< "   " << vx[i] << "   " << vy[i] << endl;
+	}
+
 	outfile.close();
 }
