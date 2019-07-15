7ba1091f1fa57e7943b1efbf3e6e3d8212a6005b
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-15 14:24:23 -0500

    Transformadas

--- Fourier.py
+++ Fourier.py
@@ -11,9 +11,33 @@ feliz= plt.imread("cara_03_grisesMF.png")
 triste= plt.imread("cara_02_grisesMF.png")
 
 #Haga filtros para realizar la imagen hibrida.
-
-
 #Haga graficas de las transformadas de fourier, de los filtros, etc. (Ilustren proceso de realización).
+
+
+#Transformada Fourier 2D.
+
+Transformadaf= fft2(feliz)
+Transformadat= fft2(triste)
+
+freqfeliz= np.fft.fftshift(feliz)
+freqtriste= np.fft.fftshift(triste)
+
+plt.figure(figsize=(10,8))
+plt.subplot(2,1,1)
+plt.plot(freqfeliz,Transformadaf, color= 'c')
+plt.xlabel("freq(Hz)")
+plt.ylabel("Tfd")
+plt.grid()
+plt.title("Transformada 2D Cara feliz")
+
+plt.subplot(2,1,2)
+plt.plot(freqtriste,Transformadaf, color= 'm')
+plt.xlabel("freq(Hz)")
+plt.ylabel("Tfd")
+plt.title("Transformada 2D Cara Triste")
+plt.subplots_adjust(hspace=0.5)
+plt.grid()
+plt.savefig("Transformadas.png")
 
 
 #Haga una imagen hibrida tal que cuando se observe de cerca, se vea a la persona seria y cuando se observe de lejos se vea a la persona sonriendo.
