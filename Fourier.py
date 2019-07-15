e0845975781926aa6895e0e4c41bb79fe91a409d
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-15 14:04:46 -0500

    leida de imagenes

--- Fourier.py
+++ Fourier.py
@@ -1,3 +1,19 @@
 import numpy as np
 import matplotlib.pylab as plt
+from scipy import fftpack
+from scipy.fftpack import fft, fftfreq,fft2
+from matplotlib.colors import LogNorm
 
+
+#Almacene los datos de las imagenes cara_02_grisesMF.png y de cara_03_grisesMF.png.
+
+feliz= plt.imread("cara_03_grisesMF.png")
+triste= plt.imread("cara_02_grisesMF.png")
+
+#Haga filtros para realizar la imagen hibrida.
+
+
+#Haga graficas de las transformadas de fourier, de los filtros, etc. (Ilustren proceso de realización).
+
+
+#Haga una imagen hibrida tal que cuando se observe de cerca, se vea a la persona seria y cuando se observe de lejos se vea a la persona sonriendo.
