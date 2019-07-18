0213ddf23cba53305bd77619867d5b55b591b6d9
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-17 21:20:10 -0500

    Ecuaciones funcionando, falta fourier

--- hw2.mk
+++ hw2.mk
@@ -1,3 +1,31 @@
 #makefile
-Resultados_hw2.pdf: 
-	pdflatex Resultados_hw2.tex
+#%.png: Fourier.py 
+	#python Fourier.py
+Posiciones.png: euler.txt eulerdt3.txt eulerdt3.txt leapfrog.txt leapfrogdt2.txt leapfrogdt3.txt rungek4.txt rungek4dt2.txt rungek4dt3.txt Plots_hw2.py ODEs.cpp
+	python Plots_hw2.py
+%.txt: a.out
+	./a.out
+a.out: ODEs.cpp
+	g++ ODEs.cpp
+
+Velocidades.png: euler.txt eulerdt3.txt eulerdt3.txt leapfrog.txt leapfrogdt2.txt leapfrogdt3.txt rungek4.txt rungek4dt2.txt rungek4dt3.txt Plots_hw2.py ODEs.cpp
+	python Plots_hw2.py
+%.txt: a.out
+	./a.out
+a.out: ODEs.cpp
+	g++ ODEs.cpp
+
+Energias.png: euler.txt eulerdt3.txt eulerdt3.txt leapfrog.txt leapfrogdt2.txt leapfrogdt3.txt rungek4.txt rungek4dt2.txt rungek4dt3.txt Plots_hw2.py ODEs.cpp
+	python Plots_hw2.py
+%.txt: a.out
+	./a.out
+a.out: ODEs.cpp
+	g++ ODEs.cpp
+
+Energiastotal.png: euler.txt eulerdt3.txt eulerdt3.txt leapfrog.txt leapfrogdt2.txt leapfrogdt3.txt rungek4.txt rungek4dt2.txt rungek4dt3.txt Plots_hw2.py ODEs.cpp
+	python Plots_hw2.py
+%.txt: a.out
+	./a.out
+a.out: ODEs.cpp
+	g++ ODEs.cpp
+
