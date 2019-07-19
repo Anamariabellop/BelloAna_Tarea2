a133e265279f128d00a2ee60c968d38bf6c2ebcc
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-18 19:04:38 -0500

    version con el pdflatex

---
+++ hw2_completo.mk
@@ -0,0 +1,10 @@
+#makefile con el pdf 
+Resultados_hw2.pdf: Resultados_hw2.tex FFT.pdf FFtIm.pdf ImProceso.pdf ImHybrid.pdf XY_met_dt.pdf VxVy_met_dt.pdf Mome_met_dt.pdf Ener_met_dt.pdf Enertotal_met_dt.pdf
+	pdflatex Resultados_hw2.tex 
+FFT.pdf FFtIm.pdf ImProceso.pdf ImHybrid.pdf XY_met_dt.pdf VxVy_met_dt.pdf Mome_met_dt.pdf Ener_met_dt.pdf Enertotal_met_dt.pdf: Fourier.py Plots_hw2.py euler.txt eulerdt2.txt eulerdt3.txt leapfrog.txt leapfrogdt2.txt leapfrogdt3.txt rungek4.txt
+	python Fourier.py
+	python Plots_hw2.py
+%.txt: a.out
+	./a.out
+a.out: ODEs.cpp
+	g++ ODEs.cpp
